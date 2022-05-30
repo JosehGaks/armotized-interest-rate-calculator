974d5b5757abd2dcc67ef8491323da8f1da88851
Joseph Gakunga <josehwilddog@gmail.com>
2022-05-30 11:32:38 +0300

    create MonthlyArmotizationElements interface

--- src/app/interest-table/interest-table.component.ts
+++ src/app/interest-table/interest-table.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface MonthlyArmotizationElements {
+  year: any;
+  principal: number;
+  interest: number;
+  total: number;
+  balance: number;
+}
+
 @Component({
   selector: 'app-interest-table',
   templateUrl: './interest-table.component.html',
-  styleUrls: ['./interest-table.component.css']
+  styleUrls: ['./interest-table.component.css'],
 })
 export class InterestTableComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
